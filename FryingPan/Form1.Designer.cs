4433e5a990718b416322648dc0a49eadf917ed18
Menno van Leeuwen <menno@vleeuwen.me>
2018-04-04 23:03:12 +0200

    First commit of this shitty thing

---
+++ FryingPan/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace FryingPan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.memoryUsage = new System.Windows.Forms.Label();
+            this.uiUpdater = new System.Windows.Forms.Timer(this.components);
+            this.memoryChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cpuUsage = new System.Windows.Forms.Label();
+            this.cpuChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.threadCount = new System.Windows.Forms.NumericUpDown();
+            this.sqrtChck = new System.Windows.Forms.CheckBox();
+            this.powChk = new System.Windows.Forms.CheckBox();
+            this.cpuName = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.memoryChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threadCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memoryUsage
+            // 
+            this.memoryUsage.AutoSize = true;
+            this.memoryUsage.Location = new System.Drawing.Point(9, 9);
+            this.memoryUsage.Name = "memoryUsage";
+            this.memoryUsage.Size = new System.Drawing.Size(65, 13);
+            this.memoryUsage.TabIndex = 0;
+            this.memoryUsage.Text = "RAM Usage";
+            // 
+            // uiUpdater
+            // 
+            this.uiUpdater.Enabled = true;
+            this.uiUpdater.Interval = 500;
+            this.uiUpdater.Tick += new System.EventHandler(this.uiUpdater_Tick);
+            // 
+            // memoryChart
+            // 
+            this.memoryChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.memoryChart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.memoryChart.Legends.Add(legend4);
+            this.memoryChart.Location = new System.Drawing.Point(12, 25);
+            this.memoryChart.Name = "memoryChart";
+            this.memoryChart.Size = new System.Drawing.Size(836, 131);
+            this.memoryChart.TabIndex = 1;
+            this.memoryChart.Text = "Memory Usage";
+            // 
+            // cpuUsage
+            // 
+            this.cpuUsage.AutoSize = true;
+            this.cpuUsage.Location = new System.Drawing.Point(706, 613);
+            this.cpuUsage.Name = "cpuUsage";
+            this.cpuUsage.Size = new System.Drawing.Size(63, 13);
+            this.cpuUsage.TabIndex = 2;
+            this.cpuUsage.Text = "CPU Usage";
+            // 
+            // cpuChart
+            // 
+            this.cpuChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea5.Name = "ChartArea1";
+            this.cpuChart.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.cpuChart.Legends.Add(legend5);
+            this.cpuChart.Location = new System.Drawing.Point(12, 162);
+            this.cpuChart.Name = "cpuChart";
+            this.cpuChart.Size = new System.Drawing.Size(836, 258);
+            this.cpuChart.TabIndex = 1;
+            this.cpuChart.Text = "CPU Usage";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 797);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Turn up the heater";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(123, 604);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 216);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 613);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Thread #";
+            // 
+            // threadCount
+            // 
+            this.threadCount.Location = new System.Drawing.Point(12, 629);
+            this.threadCount.Name = "threadCount";
+            this.threadCount.Size = new System.Drawing.Size(105, 20);
+            this.threadCount.TabIndex = 6;
+            // 
+            // sqrtChck
+            // 
+            this.sqrtChck.AutoSize = true;
+            this.sqrtChck.Checked = true;
+            this.sqrtChck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.sqrtChck.Location = new System.Drawing.Point(12, 655);
+            this.sqrtChck.Name = "sqrtChck";
+            this.sqrtChck.Size = new System.Drawing.Size(43, 17);
+            this.sqrtChck.TabIndex = 0;
+            this.sqrtChck.Text = "sqrt";
+            this.sqrtChck.UseVisualStyleBackColor = true;
+            // 
+            // powChk
+            // 
+            this.powChk.AutoSize = true;
+            this.powChk.Checked = true;
+            this.powChk.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.powChk.Location = new System.Drawing.Point(12, 678);
+            this.powChk.Name = "powChk";
+            this.powChk.Size = new System.Drawing.Size(46, 17);
+            this.powChk.TabIndex = 7;
+            this.powChk.Text = "pow";
+            this.powChk.UseVisualStyleBackColor = true;
+            // 
+            // cpuName
+            // 
+            this.cpuName.AutoSize = true;
+            this.cpuName.Location = new System.Drawing.Point(446, 613);
+            this.cpuName.Name = "cpuName";
+            this.cpuName.Size = new System.Drawing.Size(155, 39);
+            this.cpuName.TabIndex = 8;
+            this.cpuName.Text = "CPU NAME BLA BLA BLA BLA\r\n99 CORES - 99 THREADS\r\nHyperthreading: ";
+            this.cpuName.Click += new System.EventHandler(this.cpuName_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(12, 426);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(836, 172);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "CPU Temperature";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 832);
+            this.Controls.Add(this.cpuName);
+            this.Controls.Add(this.powChk);
+            this.Controls.Add(this.cpuUsage);
+            this.Controls.Add(this.sqrtChck);
+            this.Controls.Add(this.threadCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cpuChart);
+            this.Controls.Add(this.memoryChart);
+            this.Controls.Add(this.memoryUsage);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Emergency Heater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.memoryChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threadCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label memoryUsage;
+        private System.Windows.Forms.Timer uiUpdater;
+        private System.Windows.Forms.DataVisualization.Charting.Chart memoryChart;
+        private System.Windows.Forms.Label cpuUsage;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cpuChart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown threadCount;
+        private System.Windows.Forms.CheckBox sqrtChck;
+        private System.Windows.Forms.CheckBox powChk;
+        private System.Windows.Forms.Label cpuName;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ FryingPan/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FryingPan
+{
+    public class Temperature
+    {
+        public double CurrentValue { get; set; }
+        public string InstanceName { get; set; }
+        public static List<Temperature> Temperatures
+        {
+            get
+            {
+                List<Temperature> result = new List<Temperature>();
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    Double temp = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                    temp = (temp - 2732) / 10.0;
+                    result.Add(new Temperature { CurrentValue = temp, InstanceName = obj["InstanceName"].ToString() });
+                }
+                return result;
+
+            }
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public PerformanceCounter[] cpuCounter;
+        public int coreCount = 0;
+        public Series[] coreSeries;
+
+        public PerformanceCounter ramCounter;
+        public Series memorySeries;
+
+        public Series cpuTempSeries;
+
+        public long sampleTicks = 1;
+
+        public Thread[] stressThreads;
+        public ulong[] threadIterations;
+        public Label[] threadLabels;
+
+        Stopwatch stressWatch = Stopwatch.StartNew();
+        long EndingAddress = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormClosing += Form1_FormClosing;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+
+            foreach (ManagementObject share in searcher.Get())
+            {
+                cpuName.Text = share["Name"].ToString() + "\n" + share["NumberOfCores"].ToString() + " cores - " + share["ThreadCount"].ToString() + " threads\n\n";
+            }
+
+            searcher = new ManagementObjectSearcher("select * from Win32_PhysicalMemory");
+            foreach (ManagementObject share in searcher.Get())
+            {
+                EndingAddress += long.Parse(share["Capacity"].ToString()) / 1024 / 1024;
+                cpuName.Text += share["PartNumber"].ToString() + " " + share["DeviceLocator"].ToString() + " " + share["ConfiguredClockSpeed"].ToString() + "Mhz " + long.Parse(share["Capacity"].ToString()) / 1024 / 1024 / 1024 + "GB\n";
+            }
+
+            cpuName.Text += "\nTotal installed RAM " + EndingAddress / 1024 + "GB";
+
+            memorySeries = memoryChart.Series.Add("line");
+
+            memorySeries.ChartType = SeriesChartType.Line;
+            memorySeries.Name = "Memory Usage";
+
+            memoryChart.ChartAreas[0].AxisX.Title = "Time (Seconds)";
+            memoryChart.ChartAreas[0].AxisY.Title = "MegaBytes";
+            memoryChart.ChartAreas[0].AxisY.Maximum = EndingAddress;
+
+            cpuTempSeries = chart1.Series.Add("CPU Temperature");
+            cpuTempSeries.ChartType = SeriesChartType.Line;
+            
+            cpuChart.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            cpuChart.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+            cpuChart.ChartAreas[0].AxisY.Maximum = 100;
+            cpuChart.ChartAreas[0].AxisY.Title = "%";
+            cpuChart.ChartAreas[0].AxisX.Title = "Time (Seconds)";
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 120;
+            chart1.ChartAreas[0].AxisY.Title = "°C";
+            chart1.ChartAreas[0].AxisX.Title = "Time (Seconds)";
+
+            foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
+            {
+                coreCount += int.Parse(item["NumberOfCores"].ToString());
+            }
+
+            cpuCounter = new PerformanceCounter[coreCount];
+            coreSeries = new Series[coreCount];
+
+            for (int i = 0; i < coreCount; i++)
+            {
+                coreSeries[i] = cpuChart.Series.Add("CPU" + i);
+                coreSeries[i].ChartType = SeriesChartType.Line;
+
+                cpuCounter[i] = new PerformanceCounter("Processor", "% Processor Time", i.ToString());
+                Console.WriteLine(cpuCounter[i].CounterName);
+            }
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            threadCount.Value = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(button1.Text == "&Stop making it cry")
+                button1.PerformClick();
+        }
+
+        public string getCurrentCpuUsage()
+        {
+            string s = "";
+            int i = 0;
+
+            foreach (PerformanceCounter pc in cpuCounter)
+            {
+                float perc = pc.NextValue();
+                s += "CPU" + i + " - " + Math.Round(perc, 2) + "% \n";
+
+                coreSeries[i].Points.AddXY(0, perc);
+                i++;
+            }
+
+            return s;
+        }
+
+        public string getAvailableRAM()
+        {
+            return ramCounter.NextValue() + "";
+        }
+
+        public Int64 NextInt64(Random rnd)
+        {
+            var buffer = new byte[sizeof(Int64)];
+            rnd.NextBytes(buffer);
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        public object RandomIntegerCalc()
+        {
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            if (!powChk.Checked)
+            {
+                if (sqrtChck.Checked)
+                    return Math.Sqrt(NextInt64(r) * NextInt64(r) + NextInt64(r) * NextInt64(r) - NextInt64(r) * NextInt64(r) + NextInt64(r) * NextInt64(r));
+                else
+                    return NextInt64(r) * NextInt64(r) + NextInt64(r) * NextInt64(r) - NextInt64(r) * NextInt64(r) + NextInt64(r) * NextInt64(r);
+            }
+            else
+            {
+                if (sqrtChck.Checked)
+                    return Math.Sqrt(Math.Pow(r.NextDouble() * r.NextDouble(), r.NextDouble() * r.NextDouble()));
+                else
+                    return Math.Pow(r.NextDouble() * r.NextDouble(), r.NextDouble() * r.NextDouble());
+            }
+        }
+
+        private void uiUpdater_Tick(object sender, EventArgs e)
+        {
+            memorySeries.Points.AddXY(0, getAvailableRAM());
+            memoryUsage.Text = "Memory usage " + getAvailableRAM() + " MB / " + EndingAddress + "MB";
+            cpuUsage.Text = getCurrentCpuUsage();
+
+            sampleTicks++;
+
+            cpuTempSeries.Points.AddXY(0, 0);
+
+            if (threadLabels != null)
+            {
+                int i = 0;
+                foreach (Label l in threadLabels)
+                {
+                    try
+                    {
+                        l.Text = "Thread #" + i.ToString() + " iterations : " + threadIterations[i].ToString() + " Performing " +
+                      "" + (threadIterations[i] / (ulong)(stressWatch.Elapsed.Seconds)) + " Iterations/s";
+                    }
+                    catch (Exception ex) { }
+                    i++;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "&Turn up the heater")
+            {
+                button1.Text = "&Stop making it cry";
+                panel1.Controls.Clear();
+
+                sqrtChck.Enabled = false;
+                powChk.Enabled = false;
+                threadCount.Enabled = false;
+
+                stressWatch.Start();
+
+                stressThreads = new Thread[int.Parse(threadCount.Value.ToString())];
+                threadLabels = new Label[int.Parse(threadCount.Value.ToString())];
+                threadIterations = new ulong[int.Parse(threadCount.Value.ToString())];
+
+                for (int i = 0; i < threadCount.Value; i++)
+                {
+                    threadLabels[i] = new Label();
+                    stressThreads[i] = new Thread(delegate () {
+                        ulong iteration = 0;
+                        int threadNum = i;
+
+                        while (true)
+                        {
+                            threadIterations[threadNum] = iteration;
+                            RandomIntegerCalc();
+
+                            iteration++;
+                        }
+                    });
+
+                    stressThreads[i].Start();
+
+                    threadLabels[i].Size = new Size(512, 16);
+                    threadLabels[i].Location = new Point(0, i * 16);
+
+                    panel1.Controls.Add(threadLabels[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < threadCount.Value; i++)
+                {
+                    stressThreads[i].Abort();
+                }
+
+                panel1.Controls.Clear();
+                threadCount.Enabled = true;
+                sqrtChck.Enabled = true;
+                powChk.Enabled = true;
+                stressWatch.Stop();
+
+                stressThreads = null;
+                threadLabels = null;
+                threadIterations = null;
+
+                GC.Collect();
+
+                button1.Text = "&Turn up the heater";
+            }
+        }
+
+        private void cpuName_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(cpuName.Text);
+        }
+    }
+}
